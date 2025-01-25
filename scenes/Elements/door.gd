9ab301fcb5ad889450c6d5a8064e313504d9d96b
Sama5aurus <penninsamu@myvuw.ac.nz>
2025-01-25 13:30:50 +1300

    testing doors

--- scenes/Elements/door.gd
+++ scenes/Elements/door.gd
@@ -3,14 +3,14 @@ extends Area2D
 class_name Door
 
 @export var destination_level : String
-@export Literal destination_direction : String
+@export var destination_direction : String
 
 
-@onready var spwan = $Spawn
+@onready var spawn = $Spawn
 
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is CharacterBody2D:
-		SceneManager.ChangeScene(destination_level)
+		##SceneManager.ChangeScene(destination_level)
 		print("Door")
 	pass # Replace with function body.
